dac5273ad49923896da53c7c1eea656b55346b2c
Nick Piscitelli <mail@nickpiscitelli.com>
2023-01-14 18:22:09 -0500

    Pages build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/portfolio/' : undefined,
+  assetPrefix: undefined,
   images: {
     remotePatterns: [
       {
