34372fa1cbfe814d73af3eb9f8270959462eba58
Nick Piscitelli <mail@nickpiscitelli.com>
2023-01-14 18:22:37 -0500

    Fixing config to account for CNAME

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/portfolio/' : undefined,
   images: {
     remotePatterns: [
       {
